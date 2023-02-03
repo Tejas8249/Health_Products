1e56263fe611f56358484f821c133412970389a2
basu.patil581 <basu.patil581@gmail.com>
2023-02-03 17:48:28 +0530

    taken state in app to store user info

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import './App.css';
-import Header from './components/Header/Header';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Shop from "./pages/shop/Index"
-import About_us from "./pages/about-us/Index"
-import Contact_us from "./pages/contact-us/Index"
-import Login from "./pages/login/Index"
-import Sign_Up from "./pages/sign-up/Index"
-import Footer from './components/Footer/Footer';
-import Email from './pages/sign-up/Email/Email';
-import Home from './pages/home/index';
-import ButtonContainer from './pages/sign-up/Button';
-import Mobile from './pages/sign-up/MobileInputs/Index';
-import { useState } from 'react';
+import "./App.css";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./pages/shop/Index";
+import About_us from "./pages/about-us/Index";
+import Contact_us from "./pages/contact-us/Index";
+import Login from "./pages/login/Index";
+import Sign_Up from "./pages/sign-up/Index";
+import Footer from "./components/Footer/Footer";
+import Email from "./pages/sign-up/Email/Email";
+import Home from "./pages/home/index";
+import ButtonContainer from "./pages/sign-up/Button";
+import Mobile from "./pages/sign-up/MobileInputs/Index";
+import { useState } from "react";
 
 function App() {
-  const [isUser,setIsUser] = useState(true); 
+  const [isUser, setIsUser] = useState(true);
+  const [userInfo, setUserInfo] = useState({});
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path='/shop' element={<Shop />}></Route>
-          <Route path='/about-us' element={<About_us />}></Route>
-          <Route path='/contact-us' element={<Contact_us />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/sign-up' element={<Sign_Up isUser={isUser}/>}>
-            <Route index element={<ButtonContainer isUser={isUser} setIsUser={setIsUser}/>}></Route>
-            <Route path="/sign-up/email" element={<Email/>}></Route>
-            <Route path="/sign-up/mobile" element={<Mobile/>}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/about-us" element={<About_us />}></Route>
+          <Route path="/contact-us" element={<Contact_us />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/sign-up" element={<Sign_Up isUser={isUser} />}>
+            <Route
+              index
+              element={
+                <ButtonContainer isUser={isUser} setIsUser={setIsUser} />
+              }
+            ></Route>
+            <Route path="/sign-up/email" element={<Email />}></Route>
+            <Route path="/sign-up/mobile" element={<Mobile />}></Route>
             {/* <Route path="/sign-up/email" element={<Email/>}></Route> */}
           </Route>
         </Routes>
