808c0dee9355fff3d146ded23f23039414ca6193
lipa12345 <tanujalipa123@gmail.com>
2023-02-08 11:48:02 +0530

    completed home page

--- src/pages/home/components/Productrecomend2/Productrecomend2.jsx
+++ src/pages/home/components/Productrecomend2/Productrecomend2.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './Productrecomend2.css'
+import React from "react";
+import "./Productrecomend2.css";
+import { BsHeart } from "react-icons/bs";
+import StressReleiver from "../../../../Assets/Images/stressreliever.jpg";
 
 function Productrecomend2() {
   return (
-    <div>Productrecomend2</div>
-  )
+    <div className="product-2">
+      <div className="product_image2">
+        <img src={StressReleiver} alt="" />
+      </div>
+      <div className="product_info2">
+        <h4 className="pink">everything you need for your meditation</h4>
+        <h5 className="green">StressReleiver|Forever</h5>
+        <h4>StressReliever</h4>
+        <p className="product_price2">
+          <strong>$</strong>
+          <strong>123.00</strong>
+        </p>
+        <div className="product_rating2">
+          <BsHeart />
+        </div>
+      </div>
+      <button className="view-btn2">ViewYourGoal</button>
+    </div>
+  );
 }
 
-export default Productrecomend2
+export default Productrecomend2;
